8c9a27897562ee3b22bbbe039da8539e0a309bad
toweave <toweave@163.com>
2019-01-22 15:50:15 +0800

    add env

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
+console.log(6, process.env)
+console.log(7, process.env.BASE_API)
 
 Vue.config.productionTip = false
 
