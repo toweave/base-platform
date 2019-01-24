24524c4f56f5b77ee30bd07492151f2fec4c17b8
toweave <toweave@163.com>
2019-01-24 11:05:52 +0800

    empty

--- src/store/mutation-types.js
+++
@@ -1,5 +0,0 @@
-export const USER_NAME = 'USER_NAME'
-export const ACTIVE_LOADING = 'ACTIVE_LOADING'
-export const ACTIVE_LOADINGS = 'ACTIVE_LOADINGS'
-export const ACTIVE_ALERT = 'ACTIVE_ALERT'
-export const SET_CONFIG = 'SET_CONFIG'
