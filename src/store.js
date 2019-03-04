158791cc86926da5372fcf593c0a4e42b2991b1a
toweave <toweave@163.com>
2019-03-04 14:12:41 +0800

    add test

--- src/store.js
+++ src/store.js
@@ -2,8 +2,8 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import app from '@/store/app'
 import user from '@/store/user'
-console.log(5, app)
-console.log(6, user)
+console.log(5, 'store app:', app)
+console.log(6, 'store user:', user)
 
 Vue.use(Vuex)
 const debug = process.env.NODE_ENV !== 'production'
--- vue.config.js
+++ vue.config.js
@@ -67,21 +67,21 @@ module.exports = {
       .end()
   },
   // 设为false打包时不生成.map文件
-  productionSourceMap: false
+  productionSourceMap: false,
   // 这里写你调用接口的基础路径，来解决跨域，如果设置了代理，那你本地开发环境的axios的baseUrl要写为 '' ，即空字符串
   // devServer: {
   //   proxy: 'localhost:3000'
   // }
-  // devServer: {
-  //   proxy: {
-  //     '/api': {
-  //       target: 'http://www.example.org',
-  //       changeOrigin: true,
-  //       ws: true,
-  //       pathRewrite: {
-  //         '^/api': ''
-  //       }
-  //     }
-  //   }
-  // },
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://www.example.org',
+        changeOrigin: true,
+        ws: true,
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
+    }
+  }
 }
